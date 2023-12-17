e2f41df7889a5d92cf6a265913075b6ec6f00949
Paulsenik <mail@paulsenik.de>
2023-12-17 21:47:20 +0100

    updated

--- linuxSetup.bash
+++ linuxSetup.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Paulsen's Ubuntu/Debian Programm-Installscript #
-# Mainly used for already existing KDE-installation
+# Paulsen's Debian Installscript #
+# Normally used for existing KDE-installation
 # Installs my main programms with minimal configs
 #
-# https://paulsen.ooo
-# mail@paulsen.ooo
+# https://paulsenik.de
+# mail@paulsenik.de
 
 
 # Variables:
@@ -17,9 +17,11 @@ BBlue='\033[1;34m'
 folderPictures="${HOME}/Pictures"
 folderDocuments="${HOME}/Documents"
 folderInstalls="${HOME}/Installs"
-folderCode="${HOME}/Code"
+folderCode="${HOME}/Dev"
+folderRepo="$folderInstalls/paulsenik"
 ## programms
-Toolbox='jetbrains-toolbox-1.27.2.13801' #--version: https://www.jetbrains.com/toolbox-app/download/other.html
+Link_Ulauncher="https://github.com/Ulauncher/Ulauncher/releases/download/5.15.6/ulauncher_5.15.6_all.deb"
+Link_Toolbox="https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.27.2.13801.tar.gz" #--version: https://www.jetbrains.com/toolbox-app/download/other.html
 
 
 # Start
@@ -30,55 +32,41 @@ mkdir -p $folderPictures
 mkdir -p $folderInstalls
 mkdir -p $folderCode
 
-echo -e "${BPurple}> Adding 3rd party repos <${Color_Off}\n"
 
-# Repos
-sudo add-apt-repository ppa:flatpak/stable -y
-sudo add-apt-repository ppa:agornostal/ulauncher -y
-sudo add-apt-repository multiverse -y
-
-
-
-echo -e "\n\n${BPurple}> Updating.. <  ${Color_Off}\n"
 # Updating
+echo -e "\n\n${BPurple}> Updating.. <  ${Color_Off}\n"
 sudo apt update -y
 
 
-
-echo -e "\n\n${BPurple}> Setting up Discover.. < ${Color_Off}\n"
 # Discover
+echo -e "\n\n${BPurple}> Setting up Discover.. < ${Color_Off}\n"
+#-
 sudo apt install discover -y
 sudo apt install flatpak -y
+sudo apt install plasma-discover-backend-flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 
-
+## apt
 echo -e "\n\n${BPurple}> Applications & Packages < ${Color_Off}\n"
 echo -e "${BBlue}>> APT-Packages << ${Color_Off}\n"
-
-## apt
 
 ### jetbrains toolbox-app dependencies
 sudo apt install libfuse2 libxi6 libxrender1 libxtst6 mesa-utils libfontconfig libgtk-3-bin -y
 
 ### Yubikey
-sudo apt install libpam-yubico libpam-u2f ykls yubikey-luks yubikey-manager yubikey-personalization yubikey-personalization-gui scdaemon -y
+sudo apt install libpam-yubico libpam-u2f ykls yubikey-luks yubikey-manager yubikey-personalization scdaemon -y
 
 ### apt
-sudo apt install firefox -y
 sudo apt install kdeconnect -y
-sudo apt install timeshift -y
 sudo apt install default-jre default-jdk -y
-sudo apt install python3-pip -y
-sudo apt install ulauncher -y
+sudo apt install python3-pip -y # Ulauncher dependency
 sudo apt install wmctrl -y #-- Ulauncher-toggle option
-#### Fonts (Causes problems on new installs)
-#-- sudo apt install ttf-mscorefonts-installer -y
-#-- sudo fc-cache -f -v
 
 
-echo -e "\n\n${BBlue}>> Flatpak << ${Color_Off}\n"
 ## flatpak
+echo -e "\n\n${BBlue}>> Flatpak << ${Color_Off}\n"
+##-
 flatpak install com.discordapp.Discord -y --noninteractive
 flatpak install com.github.Eloston.UngoogledChromium -y --noninteractive
 flatpak install com.github.tchx84.Flatseal -y --noninteractive
@@ -95,33 +83,46 @@ flatpak install org.telegram.desktop -y --noninteractive
 flatpak install com.spotify.Client -y --noninteractive
 
 
-echo -e "\n\n${BBlue}>> Other << ${Color_Off}\n"
 ## Manual Download
+echo -e "\n\n${BBlue}>> Other << ${Color_Off}\n"
+cd $folderInstalls
 
 ### Ulauncher
-pip3 install requests --user
+wget -nv -nc --show-progress --progress="bar" $Link_Ulauncher
+sudo apt install ./ulauncher_5.15.6_all.deb -y
+pip3 install requests --user #-- pip-Dependencies
 
 ### Jetbrains-Toolbox
-cd $folderInstalls
-wget -nv -nc --show-progress --progress="bar" "https://download.jetbrains.com/toolbox/${Toolbox}.tar.gz"
+wget -nv -nc --show-progress --progress="bar" $Link_Toolbox
 tar -xf ${Toolbox}.tar.gz
 cd $Toolbox
 ./jetbrains-toolbox --minimize
 
 
-echo -e "\n\n${BPurple}> Configs < ${Color_Off}\n"
 # Config & Files
+echo -e "\n\n${BPurple}> Configs < ${Color_Off}\n"
+## Repo-Files
 cd $folderInstalls
-rm -rf realPaulsen
-git clone https://github.com/realPaulsen/realPaulsen.git
-cp -rf realPaulsen/* ${HOME}
+rm -rf paulsenik
+git clone https://github.com/paulsenik/paulsenik.git
+
+## Ulauncher
+cp -r ${folderRepo}/.config/ulauncher ${HOME}/.config
+## Ryolith
+cp -r ${folderRepo}/.ryolith ${HOME}
+cp -r ${folderRepo}/.local/share/applications/Ryolith.desktop ${HOME}/.local/share/applications/
 
 
-# Info
-echo -e "\n\n${BPurple}Things ToDo manually: ${Color_Off}\n"
-echo -e "${BBlue}Set wallpaper from ${folderPictures}"
-echo -e "${BBlue}If you are using KDE apply settings from ~/.config/REAMDE.md and import ${BPurple}kde-shortcuts.kksrc ${BBlue}into Settings/Shortcuts."
+## KDE
+if command -v kwriteconfig5 >/dev/null 2>&1; then
+  echo -e "\n\n${BPurple}Setting kde5-configs: ${Color_Off}\n"
+  echo -e "${BBlue}import ${BPurple}kde-shortcuts.kksrc ${BBlue}into Settings/Shortcuts.${Color_Off}"
+
+  kwriteconfig5 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' "$folderInstalls/paulsenik/Pictures/Profile/Phoenix 1.0/phoenix_v1.0_UHD-2.png"
+
+else
+  echo "${BBlue}no kde5-installation found!"
+fi
 
 
 exit
-
