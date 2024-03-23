fc2f0aeb723870d1b7cc6411a622d74c17e87b94
Paulsenik <mail@paulsenik.de>
2024-03-23 09:22:31 +0100

    added confirmation

--- linuxSetup.bash
+++ linuxSetup.bash
@@ -20,7 +20,7 @@ folderInstalls="${HOME}/Installs"
 folderCode="${HOME}/Dev"
 folderRepo="$folderInstalls/paulsenik"
 ## programms
-Toolbox="jetbrains-toolbox-2.1.2.18853"
+Toolbox="jetbrains-toolbox-2.2.3.20090"
 KrohnkiteV="0.8.1"
 Link_Toolbox="https://download.jetbrains.com/toolbox/${Toolbox}.tar.gz" #--version: https://www.jetbrains.com/toolbox-app/download/other.html
 Link_Krohnkite="https://github.com/esjeon/krohnkite/releases/download/v${KrohnkiteV}/krohnkite-${KrohnkiteV}.kwinscript"
@@ -38,109 +38,163 @@ mkdir -p $folderCode
 
 # Updating
 echo -e "\n\n${BPurple}> Updating.. <  ${Color_Off}\n"
-sudo apt update -y
+
+echo "Proceed? [y/N]: "
+read confirm
+if [[ $confirm == y* ]]; then
+  sudo apt update -y
+else
+    echo "Skipping..."
+fi
 
 
 # Discover
 echo -e "\n\n${BPurple}> Setting up Discover.. < ${Color_Off}\n"
 #-
-sudo apt install discover -y
-sudo apt install flatpak -y
-sudo apt install plasma-discover-backend-flatpak -y
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo "Proceed? [y/N]: "
+read confirm
+if [[ $confirm == y* ]]; then
+  sudo apt install discover -y
+  sudo apt install flatpak -y
+  sudo apt install plasma-discover-backend-flatpak -y
+  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+else
+    echo "Skipping..."
+fi
 
 
 ## apt
 echo -e "\n\n${BPurple}> Applications & Packages < ${Color_Off}\n"
 echo -e "${BBlue}>> APT-Packages << ${Color_Off}\n"
+echo "Proceed? [y/N]: "
+read confirm
+if [[ $confirm == y* ]]; then
 
-### jetbrains toolbox-app dependencies
-sudo apt install libfuse2 libxi6 libxrender1 libxtst6 mesa-utils libfontconfig libgtk-3-bin -y
+  ### jetbrains toolbox-app dependencies
+  sudo apt install libfuse2 libxi6 libxrender1 libxtst6 mesa-utils libfontconfig libgtk-3-bin -y
 
-### Yubikey
-sudo apt install libpam-yubico libpam-u2f ykls yubikey-luks yubikey-manager yubikey-personalization scdaemon -y
+  ### Yubikey
+  sudo apt install libpam-yubico libpam-u2f ykls yubikey-luks yubikey-manager yubikey-personalization scdaemon -y
 
-### apt
-sudo apt install kdeconnect -y
-sudo apt install default-jre default-jdk -y
-sudo apt install python3-pip -y # Ulauncher dependency
-sudo apt install wmctrl -y #-- Ulauncher-toggle option
+  ### apt
+  sudo apt install kdeconnect -y
+  sudo apt install default-jre default-jdk -y
+  sudo apt install python3-pip -y # Ulauncher dependency
+  sudo apt install wmctrl -y #-- Ulauncher-toggle option
+
+else
+    echo "Skipping..."
+fi
 
 
 ## flatpak
 echo -e "\n\n${BBlue}>> Flatpak << ${Color_Off}\n"
 ##-
-flatpak install com.discordapp.Discord -y --noninteractive
-flatpak install com.github.Eloston.UngoogledChromium -y --noninteractive
-flatpak install com.github.tchx84.Flatseal -y --noninteractive
-flatpak install com.nextcloud.desktopclient.nextcloud -y --noninteractive
-flatpak install im.riot.Riot -y --noninteractive
-flatpak install md.obsidian.Obsidian -y --noninteractive
-flatpak install org.kde.filelight -y --noninteractive
-flatpak install org.kde.kcalc -y --noninteractive
-flatpak install org.kde.kcolorchooser -y --noninteractive
-flatpak install org.kde.krita -y --noninteractive
-flatpak install org.mozilla.Thunderbird -y --noninteractive
-flatpak install org.onlyoffice.desktopeditors -y --noninteractive
-flatpak install org.telegram.desktop -y --noninteractive
-flatpak install com.spotify.Client -y --noninteractive
+echo "Proceed? [y/N]: "
+read confirm
+if [[ $confirm == y* ]]; then
+
+  flatpak install com.github.Eloston.UngoogledChromium -y --noninteractive
+  flatpak install com.discordapp.Discord -y --noninteractive
+  flatpak install com.github.tchx84.Flatseal -y --noninteractive
+  flatpak install com.nextcloud.desktopclient.nextcloud -y --noninteractive
+  flatpak install com.spotify.Client -y --noninteractive
+  flatpak install com.teamspeak.TeamSpeak -y --noninteractive
+  flatpak install im.riot.Riot -y --noninteractive
+  flatpak install md.obsidian.Obsidian -y --noninteractive
+  flatpak install org.flameshot.Flameshot -y --noninteractive
+  flatpak install org.kde.filelight -y --noninteractive
+  flatpak install org.kde.kcalc -y --noninteractive
+  flatpak install org.kde.kcolorchooser -y --noninteractive
+  flatpak install org.kde.krita -y --noninteractive
+  flatpak install org.mozilla.Thunderbird -y --noninteractive
+  flatpak install org.onlyoffice.desktopeditors -y --noninteractive
+  flatpak install org.telegram.desktop -y --noninteractive
+
+else
+    echo "Skipping..."
+fi
 
 
 ## Manual Download
 echo -e "\n\n${BBlue}>> Other << ${Color_Off}\n"
 cd $folderInstalls
 
-### Ulauncher
-wget -nv -nc --show-progress --progress="bar" $Link_Ulauncher
-sudo apt install ./ulauncher_5.15.6_all.deb -y
-pip3 install requests --user #-- pip-Dependencies
+echo "Proceed? [y/N]: "
+read confirm
+if [[ $confirm == y* ]]; then
 
-### Jetbrains-Toolbox
-wget -nv -nc --show-progress --progress="bar" $Link_Toolbox
-tar -xf ${Toolbox}.tar.gz
-cd $Toolbox
-./jetbrains-toolbox --minimize
+  ### Ulauncher
+  wget -nv -nc --show-progress --progress="bar" $Link_Ulauncher
+  sudo apt install ./ulauncher_5.15.6_all.deb -y
+  pip3 install requests --user #-- pip-Dependencies
+
+  ### Jetbrains-Toolbox
+  wget -nv -nc --show-progress --progress="bar" $Link_Toolbox
+  tar -xf ${Toolbox}.tar.gz
+  cd $Toolbox
+  ./jetbrains-toolbox --minimize
+
+else
+    echo "Skipping..."
+fi
 
 
 # Config & Files
 echo -e "\n\n${BPurple}> Configs < ${Color_Off}\n"
-## Repo-Files
-cd $folderInstalls
-rm -rf paulsenik
-git clone https://github.com/paulsenik/paulsenik.git
 
-## Ulauncher
-cp -r -u ${folderRepo}/.config/ulauncher ${HOME}/.config
-## Ryolith
-cp -r -u ${folderRepo}/.ryolith ${HOME}
-## Inputsink
-cp -r -u ${folderRepo}/.inputsink ${HOME}
-## Konsole
-cp -r -u ${folderRepo}/.local/share/konsole/ ${HOME}/.local/share/
+echo "Proceed? [y/N]: "
+read confirm
+if [[ $confirm == y* ]]; then
 
-## Desktop-Links
-cp -r -u ${folderRepo}/.local/share/applications/ ${HOME}/.local/share/
-##- change Desktop-Links (https://askubuntu.com/questions/20414/find-and-replace-text-within-a-file-using-commands)
-##- Replaces _HOME_ with actual home-folder
-sed -i "s/_HOME_/\/home\/${USER}/g" ${HOME}/.local/share/applications/*
+  ## Repo-Files
+  cd $folderInstalls
+  rm -rf paulsenik
+  git clone https://github.com/paulsenik/paulsenik.git
 
+  ## Ulauncher
+  cp -r -u ${folderRepo}/.config/ulauncher ${HOME}/.config
+  ## Ryolith
+  cp -r -u ${folderRepo}/.ryolith ${HOME}
+  ## Inputsink
+  cp -r -u ${folderRepo}/.inputsink ${HOME}
+  ## Konsole
+  cp -r -u ${folderRepo}/.local/share/konsole/ ${HOME}/.local/share/
+
+  ## Desktop-Links
+  cp -r -u ${folderRepo}/.local/share/applications/ ${HOME}/.local/share/
+  ##- change Desktop-Links (https://askubuntu.com/questions/20414/find-and-replace-text-within-a-file-using-commands)
+  ##- Replaces _HOME_ with actual home-folder
+  sed -i "s/_HOME_/\/home\/${USER}/g" ${HOME}/.local/share/applications/*
+
+else
+    echo "Skipping..."
+fi
 
 ## KDE (check if kde5)
 if command -v kwriteconfig5 >/dev/null 2>&1; then
   echo -e "\n\n${BPurple}Setting kde5-configs: ${Color_Off}\n"
   echo -e "${BBlue}import ${BPurple}kde-shortcuts.kksrc ${BBlue}into Settings/Shortcuts.${Color_Off}"
 
-  ### Background
-  kwriteconfig5 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' "$folderInstalls/paulsenik/Pictures/Profile/Phoenix 1.0/phoenix_v1.0_UHD-2.png"
+  echo "Proceed? [y/N]: "
+  read confirm
+  if [[ $confirm == y* ]]; then
 
-  ### Krohnkite
-  cd $folderInstalls
-  wget -nv -nc --show-progress --progress="bar" $Link_Krohnkite
-  plasmapkg2 -t kwinscript -i "krohnkite-${KrohnkiteV}.kwinscript"
-  ###- Enable User Config
-  mkdir -p "$HOME/.local/share/kservices5/"
-  ln -s "$HOME/.local/share/kwin/scripts/krohnkite/metadata.desktop" "$HOME/.local/share/kservices5/krohnkite.desktop"
+    ### Background
+    kwriteconfig5 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' "$folderInstalls/paulsenik/Pictures/Profile/Phoenix 1.0/phoenix_v1.0_UHD-2.png"
 
+    ### Krohnkite
+    cd $folderInstalls
+    wget -nv -nc --show-progress --progress="bar" $Link_Krohnkite
+    plasmapkg2 -t kwinscript -i "krohnkite-${KrohnkiteV}.kwinscript"
+    ###- Enable User Config
+    mkdir -p "$HOME/.local/share/kservices5/"
+    ln -s "$HOME/.local/share/kwin/scripts/krohnkite/metadata.desktop" "$HOME/.local/share/kservices5/krohnkite.desktop"
+
+  else
+      echo "Skipping..."
+  fi
 else
   echo "${BBlue}No kde5-installation found!"
 fi
