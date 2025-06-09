b85fb9aa88f38395c8a06c2102155ce163e109d6
Paulsenik <mail@paulsenik.de>
2025-06-10 01:55:40 +0200

    added simple ansible roles

---
+++ ansible/run.bash
@@ -0,0 +1,9 @@
+
+# Debian Install Ansible - https://docs.ansible.com/ansible/latest/installation_guide/installation_distros.html#installing-ansible-on-debian
+UBUNTU_CODENAME=jammy
+wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
+sudo apt update && sudo apt install ansible
+
+
+ansible-playbook -i ansible/hosts -l pc ansible/playbook.yaml
